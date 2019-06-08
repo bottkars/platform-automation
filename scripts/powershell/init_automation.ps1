31820447252aea86b831d845c934ec7477e7b66d
PCF Developer <pcfuser@labbuildr.com>
2019-06-08 10:38:53 +0200

    interpolatiin

--- scripts/powershell/init_automation.ps1
+++ scripts/powershell/init_automation.ps1
@@ -53,23 +53,28 @@ $DIRECTOR_FOUNDATION = Get-Content $DIRECTOR_FOUNDATION_FILE | ConvertFrom-Json
 $FOUNDATION=$DIRECTOR_FOUNDATION.PCF_SUBDOMAIN_NAME
 ($CA_CERT | Out-String) | set-content $HOME\credhub_ca_cert
 
+credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/$($FOUNDATION)/secret_access_key /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/pivnet_token /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/$($FOUNDATION)/pivnet-token /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
+
+
+
 credhub set /name:/concourse/main/$($FOUNDATION)/tenant-id /type:value --value $($env_vars.AZURE_TENANT_ID)
 credhub set /name:/concourse/main/$($FOUNDATION)/client-id /type:value --value $($env_vars.AZURE_CLIENT_ID)
 credhub set /name:/concourse/main/$($FOUNDATION)/client-secret /type:value --value $($env_vars.AZURE_CLIENT_SECRET)
 credhub set /name:/concourse/main/$($FOUNDATION)/subscription-id /type:value --value $($env_vars.AZURE_SUBSCRIPTION_ID)
-credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/credhub-client /type:value --value $CLIENT_NAME
 credhub set /name:/concourse/main/credhub-secret /type:value --value $CREDHUB_PASSWORD
 credhub set /name:/concourse/main/credhub-server /type:value --value $CREDHUB_URL
 credhub set /name:/concourse/main/credhub-ca-cert /type:certificate /root:"$HOME\credhub_ca_cert"
 credhub set /name:/concourse/main/$($FOUNDATION)/access_key_id /type:value --value s3admin
-credhub set /name:/concourse/main/$($FOUNDATION)/secret_access_key /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_tasks /type:value --value tasks
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_image /type:value --value image
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_products /type:value --value pivnet.products
 
 
-credhub set /name:/concourse/main/pivnet_token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/plat-auto-pipes-deploy-key /type:ssh `
          /private:$HOME\.ssh\git_deploy `
          /public:$HOME\.ssh\git_deploy.pub
@@ -87,12 +92,10 @@ credhub set /name:/concourse/main/$($FOUNDATION)/s3_endpoint /type:value --value
 credhub set /name:/concourse/main/$($FOUNDATION)/s3_region_name /type:value /value:region
 
 
-credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
-credhub set /name:/concourse/main/$($FOUNDATION)/pivnet-token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 
 # s3 connection
 credhub set /name:/concourse/main/buckets_pivnet_products /type:value --value pivnet.products
-credhub set /name:/concourse/main/secret_access_key /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/secret_access_key /type:value --value $($env_vars.EMC_PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/access_key_id /type:value --value s3admin
 
 
