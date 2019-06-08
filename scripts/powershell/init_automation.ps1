46fd3579fe77c0c066d2152bb0469492f5a9af61
PCF Developer <pcfuser@labbuildr.com>
2019-06-08 09:59:38 +0200

    latest 1

--- scripts/powershell/init_automation.ps1
+++ scripts/powershell/init_automation.ps1
@@ -51,7 +51,7 @@ $DIRECTOR_FOUNDATION_FILE="$HOME\director_pcf.json"
 
 $DIRECTOR_FOUNDATION = Get-Content $DIRECTOR_FOUNDATION_FILE | ConvertFrom-Json
 $FOUNDATION=$DIRECTOR_FOUNDATION.PCF_SUBDOMAIN_NAME
-
+($CA_CERT | Out-String) | set-content $HOME\credhub_ca_cert
 
 credhub set /name:/concourse/main/$($FOUNDATION)/tenant-id /type:value --value $($env_vars.AZURE_TENANT_ID)
 credhub set /name:/concourse/main/$($FOUNDATION)/client-id /type:value --value $($env_vars.AZURE_CLIENT_ID)
@@ -61,15 +61,31 @@ credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --valu
 credhub set /name:/concourse/main/credhub-client /type:value --value $CLIENT_NAME
 credhub set /name:/concourse/main/credhub-secret /type:value --value $CREDHUB_PASSWORD
 credhub set /name:/concourse/main/credhub-server /type:value --value $CREDHUB_URL
-
+credhub set /name:/concourse/main/credhub-ca-cert /type:certificate /root:"$HOME\credhub_ca_cert"
 credhub set /name:/concourse/main/$($FOUNDATION)/access_key_id /type:value --value s3admin
 credhub set /name:/concourse/main/$($FOUNDATION)/secret_access_key /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_tasks /type:value --value tasks
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_image /type:value --value image
 credhub set /name:/concourse/main/$($FOUNDATION)/buckets_pivnet_products /type:value --value pivnet.products
 
+
+credhub set /name:/concourse/main/pivnet_token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
+credhub set /name:/concourse/main/plat-auto-pipes-deploy-key /type:ssh `
+         /private:$HOME\.ssh\git_deploy `
+         /public:$HOME\.ssh\git_deploy.pub
+
+
+credhub set /name:/concourse/main/buckets_pivnet_tasks /type:value --value tasks
+credhub set /name:/concourse/main/buckets_pivnet_image /type:value --value image
+credhub set /name:/concourse/main/buckets_pivnet_products /type:value --value pivnet.products
+
+
+
+# credhub set /name:/concourse/main/s3_endpoint /type:value --value "http://minio.$($PCF_SUBDOMAIN_NAME).$($PCF_DOMAIN_NAME):9000"
+# credhub set /name:/concourse/main/s3_region_name /type:value /value:region
 credhub set /name:/concourse/main/$($FOUNDATION)/s3_endpoint /type:value --value "http://minio.$($PCF_SUBDOMAIN_NAME).$($PCF_DOMAIN_NAME):9000"
 credhub set /name:/concourse/main/$($FOUNDATION)/s3_region_name /type:value /value:region
+
 
 credhub set /name:/concourse/main/$($FOUNDATION)/pivnet_token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
 credhub set /name:/concourse/main/$($FOUNDATION)/pivnet-token /type:value --value $($env_vars.PIVNET_UAA_TOKEN)
--- walk.ps1
+++ walk.ps1
@@ -2,5 +2,7 @@ $PIPELINE="azurestack_asdk"
 
 fly --target plane login --concourse-url $FLY_URL -k
 
-fly -t plane set-pipeline -c pipeline_s3.yml -l vars_pcfdemo.yml -p $PIPELINE
+fly -t plane set-pipeline -c pipeline_pcfdemo.yml -l vars_pcfdemo.yml -p $PIPELINE
 fly -t plane up -p $PIPELINE
+
+fly -t plane dp -p $PIPELINE
