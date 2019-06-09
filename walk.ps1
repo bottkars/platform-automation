297013691d44742f281d05f0b9fcdd47f12686ed
PCF Developer <pcfuser@labbuildr.com>
2019-06-09 08:37:17 +0200

    added p-spring

--- walk.ps1
+++ walk.ps1
@@ -1,5 +1,5 @@
 $PIPELINE="azurestack_asdk"
-
+$FLY_URL="https://plane.control.local.azurestack.external"
 fly --target plane login --concourse-url $FLY_URL -k
 
 fly -t plane set-pipeline -c pipeline_pcfdemo.yml -l vars_pcfdemo.yml -p $PIPELINE
