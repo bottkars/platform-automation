fbe2585cee7f611d170309decf27172a1732aa7d
PCF Developer <pcfuser@labbuildr.com>
2019-06-29 09:39:17 +0200

    latest

--- walk.ps1
+++ walk.ps1
@@ -2,7 +2,7 @@ $PIPELINE="azurestack_asdk"
 $FLY_URL="https://plane.control.local.azurestack.external"
 fly --target plane login --concourse-url $FLY_URL -k
 
-fly -t plane set-pipeline -c pipeline_pcfdemo.yml -l vars_pcfdemo.yml -p $PIPELINE
+fly -t plane set-pipeline -c .\pipeline_azurestack.yml -l vars_$($PIPELINE).yml -p $PIPELINE
 fly -t plane up -p $PIPELINE
 
 fly -t plane dp -p $PIPELINE
