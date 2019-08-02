fa56629ba64844e5e8ad627f6482dbb94c05ab73
PCF Developer <pcfuser@labbuildr.com>
2019-08-02 16:29:11 +0200

    befor making public

--- scripts/connect_stackpoc.sh
+++ scripts/connect_stackpoc.sh
@@ -9,11 +9,3 @@ unset CREDHUB_PROXY
 unset CREDHUB_SERVER
 unset CREDHUB_SECRET
 
-credhub login -s "${CREDHUB_URL}" --client-name "${CLIENT_NAME}" --client-secret "${credhub_password}" --ca-cert "${CA_CERT}"
-
-
- credhub set \
-         --name /concourse/stackpoc-demo/plat-auto-pipes-deploy-key \
-         --type ssh \
-         --private /Users/bottk/.ssh/git_deploy \
-         --public /Users/bottk/.ssh/git_deploy.pub
