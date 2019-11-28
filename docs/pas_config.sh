bf31fc9a63be82161bc805709024f92609fa9ae3
bottkars <karsten.bott@bottnet.de>
2019-11-28 10:09:23 +0100

    pas config sterps

--- docs/pas_config.sh
+++ docs/pas_config.sh
@@ -5,16 +5,20 @@
 
 # retrieving uaa password
 ENV=${HOME}/workspace/pcfsc2_azurestack_config/pcfsc2/env/env.yml
+uaac target https://uaa.sys.pcfsc2.sc2.azurestack-rd.cf-app.com
+
+ENV=${HOME}/workspace/pcfdemo_asdk_config/pcfdemo/env/env.yml
+uaac target https://uaa.sys.pcfdemo.local.azurestack.external
 
 UAA_ADMIN_CREDENTIALS=$(om --env ${ENV} credentials -p cf -c .uaa.admin_client_credentials -f password)
-uaac target https://uaa.sys.pcfsc2.sc2.azurestack-rd.cf-app.com
+
 uaac token client get admin -s ${UAA_ADMIN_CREDENTIALS}
 
 
 uaac group map --name scim.read "fd570d0b-ae8b-45f8-871e-7e40ef426dd8" --origin labbuildr
 uaac group map --name scim.write "fd570d0b-ae8b-45f8-871e-7e40ef426dd8" --origin labbuildr
 uaac group map --name cloud_controller.admin "fd570d0b-ae8b-45f8-871e-7e40ef426dd8" --origin labbuildr
-
+uaac group map --name healthwatch.read "fd570d0b-ae8b-45f8-871e-7e40ef426dd8" --origin labbuildr
 ## wavefront
 uaac -t member add doppler.firehose wavefront_nozzle
 uaac -t user add wavefront-nozzle --password Thyph00n# --emails na
