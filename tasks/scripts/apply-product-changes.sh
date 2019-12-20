b34aff581b6c5c93865ea74927cbade01376a3a2
bottkars <karsten.bott@bottnet.de>
2019-12-20 11:19:54 +0100

    added somme additional tasks

--- tasks/scripts/apply-product-changes.sh
+++ tasks/scripts/apply-product-changes.sh
@@ -1,6 +1,7 @@
-
+#!/usr/bin/env bash
+# code_snippet apply-changes-script start bash
 cat /var/version && echo ""
 set -eux
 om --env env/"${ENV_FILE}" apply-changes \
-  ----product-name ${PRODUCT_NAME}
+  --product-name ${PRODUCT_NAME} \
   --reattach
