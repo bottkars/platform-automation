178ce9890c8bf87932d276e86e204a2e75e22a94
Karsten Bott <karsten.bott@bottnet.de>
2020-01-30 11:45:38 +0100

    adding minio client

--- scripts/test_releases.sh
+++ scripts/test_releases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+VERSIONS_FILE="${HOME}/workspace/pcf-controlplane-azurestack/templates/versions.yml"
 function get_latest_release() {
-curl -u ${GITUSER} --silent "https://api.github.com/repos/$1/tags"  | jq -r '[.[] | select(.name!="v1")] | .[0].name'
+curl -u "${GIT_USERNAME}:${GIT_TOKEN}" --silent "https://api.github.com/repos/$1/tags"  | jq -r '[.[] | select(.name!="v1")] | .[0].name'
 }
 
 RELEASES="cloudfoundry, garden-runc-release, 
@@ -18,10 +18,18 @@ RELEASES="cloudfoundry, garden-runc-release,
             concourse, concourse-bosh-release, 
             pivotal, credhub-release,
             minio, minio-boshrelease"
-echo $RELEASES
-unset REPO RELEASE VERSIONS
+unset REPO RELEASE VERSION
 set -u
 while IFS=", " read -r REPO RELEASE; do
     VERSION=$(get_latest_release "${REPO}/${RELEASE}")
-    echo "${RELEASE}: \"${VERSION//v}\""
-done <<< "${RELEASES}"            
+    echo "Replacing ${RELEASE} version with \"${VERSION//v}\" in $VERSIONS_FILE"
+    sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//v}\"/" $VERSIONS_FILE
+done <<< "${RELEASES}"    
+
+VERSION=$(curl -u "${GIT_USERNAME}:${GIT_TOKEN}" --silent "https://api.github.com/repos/cloudfoundry/bosh-linux-stemcell-builder/tags"  | jq -r '[.[] | select(.name | contains("621."))] | .[0].name')
+RELEASE=stemcell-release
+echo "Replacing ${RELEASE} version with \"${VERSION//ubuntu-xenial\/v}\" in $VERSIONS_FILE"
+sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//ubuntu-xenial\/v}\"/" $VERSIONS_FILE
+
+# https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=1.19.10
+# https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=1.19.10
