1013433e866d2861de6cd7fd9da2c832b2d28bfb
Karsten Bott <karsten.bott@bottnet.de>
2020-01-30 14:26:06 +0100

    latesr

--- scripts/test_releases.sh
+++ scripts/test_releases.sh
@@ -21,15 +21,28 @@ RELEASES="cloudfoundry, garden-runc-release,
 unset REPO RELEASE VERSION
 set -u
 while IFS=", " read -r REPO RELEASE; do
+    OLD_VERSION=$(grep -A0 ${RELEASE} $VERSIONS_FILE | cut -d ':' -f2 | tr -d ' "')
     VERSION=$(get_latest_release "${REPO}/${RELEASE}")
-    echo "Replacing ${RELEASE} version with \"${VERSION//v}\" in $VERSIONS_FILE"
-    sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//v}\"/" $VERSIONS_FILE
+    if [[ ${VERSION//v} != $OLD_VERSION ]]
+        then
+        echo "Replacing ${RELEASE} version ${OLD_VERSION} with ${VERSION//v} in $VERSIONS_FILE"
+        sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//v}\"/" $VERSIONS_FILE
+    else
+        echo "Already at version ${VERSION//v}"
+    fi
 done <<< "${RELEASES}"    
 
 VERSION=$(curl -u "${GIT_USERNAME}:${GIT_TOKEN}" --silent "https://api.github.com/repos/cloudfoundry/bosh-linux-stemcell-builder/tags"  | jq -r '[.[] | select(.name | contains("621."))] | .[0].name')
 RELEASE=stemcell-release
-echo "Replacing ${RELEASE} version with \"${VERSION//ubuntu-xenial\/v}\" in $VERSIONS_FILE"
-sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//ubuntu-xenial\/v}\"/" $VERSIONS_FILE
+    OLD_VERSION=$(grep -A0 ${RELEASE} $VERSIONS_FILE | cut -d ':' -f2 | tr -d ' "')
+    if [[ ${VERSION//ubuntu-xenial\/v} != ${OLD_VERSION} ]]
+        then
+        echo "Replacing ${RELEASE} version ${OLD_VERSION} with \"${VERSION//ubuntu-xenial\/v}\" in $VERSIONS_FILE"
+        sed -i "/${RELEASE}/s/.*/${RELEASE}: \"${VERSION//ubuntu-xenial\/v}\"/" $VERSIONS_FILE
+    else
+        echo "Already at Stemcell version ${VERSION//ubuntu-xenial\/v}"
+    fi
+
 
 # https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=1.19.10
 # https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=1.19.10
